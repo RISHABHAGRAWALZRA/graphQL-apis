1d869922a7420f5e42b5602eb1ae827343d26463
rishabhagrawalzra <rishabhagrawalzra@gmail.com>
2023-03-08 12:53:07 +0530

    6: connected creator:events and createdEvents:user

--- graphql/resolver/resolver.js
+++ graphql/resolver/resolver.js
@@ -3,41 +3,71 @@ const User = require('../../models/user')
 
 const bcrypt = require('bcryptjs')
 
+const events = async (eventIds) => {
+    try {
+        const foundEvents = await Event.find({ _id: { $in: eventIds } })
+        return foundEvents.map(event => {
+            return { ...event._doc, _id: event.id, creator: user.bind(this, event.creator) }
+        })
+    } catch (err) {
+        throw err
+    }
+}
+
+const user = async (userId) => {
+    try {
+        const foundUser = await User.findById(userId)
+        return { ...foundUser._doc, _id: foundUser.id, createdEvents: events.bind(this, foundUser._doc.createdEvents) }
+    } catch (err) {
+        throw err
+    }
+}
+
 module.exports = {
-    event : async() => {
+    event: async () => {
         try {
             const events = await Event.find()
             return events.map(event => {
-                return {...event._doc, _id: event.id, date: new Date(event._doc.date).toISOString()}
+                return { ...event._doc, _id: event.id, date: new Date(event._doc.date).toISOString(), creator: user.bind(this, event._doc.creator) }
             })
         } catch (err) {
-            console.log(err)   
+            console.log(err)
+            throw err
         }
     },
 
-    createEvent : async(args) => {
+    createEvent: async (args) => {
         try {
             const event = new Event({
                 title: args.input.title,
                 description: args.input.description,
                 price: args.input.price,
                 date: new Date().toISOString(),
+                creator: '640749d0ef23832673065c68'
             })
 
-            const result = await event.save()
+            const foundUser = await User.findById('640749d0ef23832673065c68')
 
-            console.log(result)
-            return {...result._doc, _id: result._doc._id.toString(), date: new Date(result._doc.date).toISOString()}
+            if (foundUser) {
+                const result = await event.save()
+                foundUser.createdEvents.push(event)
+                await foundUser.save()
+                console.log(result)
+                return { ...result._doc, _id: result._doc._id.toString(), date: new Date(result._doc.date).toISOString(), creator: user.bind(this, result._doc.creator) }
+            } else {
+                throw new Error("User not found")
+            }
 
         } catch (err) {
             console.log(err)
-        }       
+            throw err
+        }
     },
 
-    createUser : async(args) => {
+    createUser: async (args) => {
         try {
-            const existingUser = await User.findOne({email:args.input.email})
-            if(existingUser){
+            const existingUser = await User.findOne({ email: args.input.email })
+            if (existingUser) {
                 throw new Error("User is already exists")
             }
 
@@ -48,7 +78,7 @@ module.exports = {
             })
             const result = await newUser.save()
 
-            return {...result._doc, password: null, _id: result.id}
+            return { ...result._doc, password: null, _id: result.id }
         } catch (err) {
             console.log(err)
             throw err
--- graphql/schema/schema.js
+++ graphql/schema/schema.js
@@ -1,4 +1,4 @@
-const { buildSchema }  = require('graphql')
+const { buildSchema } = require('graphql')
 
 module.exports = buildSchema(`
 
@@ -8,12 +8,14 @@ module.exports = buildSchema(`
             description: String
             price: Float!
             date: String!
+            creator: User!
         }
 
         type User{
             _id: ID
             email: String!
             password: String
+            createdEvents: [Event!]
         }
 
         input EventInput{
